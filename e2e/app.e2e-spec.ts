c8fe8312fe753f78f354b631dd709700d578851f
Harish Anchu <harishanchu@gmail.com>
2018-04-08 17:24:45 +0530

    Updates

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('my-deck App', () => {
+describe('Workplace App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/globals.ts
+++ src/app/globals.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-  appName = 'My deck';
+  appName = 'Workplace';
 }
