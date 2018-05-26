6fae86696b12cd24f29536996045b74f8141921f
Harish Anchu <harishanchu@gmail.com>
2018-05-26 23:58:21 +0530

    Updates

--- src/app/pages/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/pages/admin/admin-dashboard/admin-dashboard.component.ts
@@ -16,30 +16,42 @@ export interface Chart {
   styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent implements OnInit {
-  private hoursChart = <Chart>{
+  private hoursChart = <Chart> {
     type: 'Line',
+    options: {
+      lineSmooth: Chartist.Interpolation.cardinal({
+        tension: 0
+      }),
+      // low: 0,
+      // high: 50,
+      chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
+    },
     data: {
-      'labels': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      'labels': ['M', 'T', 'W', 'T', 'F'],
       'series': [
         [12, 9, 7, 8, 5]
       ]
     }
   };
 
-  private resourceAllocationChart = <Chart>{
-    type: 'Line',
+  private resourceAllocationChart = <Chart> {
     data: {
-      'labels': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-      'series': [
-        [2, 1, 3.5, 7, 3]
-      ]
-    }
+      series: [20, 10, 30, 40]
+    },
+    options: {
+      donut: true,
+      donutWidth: 40,
+      donutSolid: true,
+      startAngle: 270,
+      showLabel: true
+    },
+    type: 'Pie'
   };
 
-  private tasksChart = <Chart>{
+  private tasksChart = <Chart> {
     type: 'Line',
     data: {
-      'labels': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      'labels': ['M', 'T', 'W', 'T', 'F'],
       'series': [
         [1, 3, 4, 5, 6]
       ]
